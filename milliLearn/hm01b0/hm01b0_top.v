345b0976cdbb788d7a719e91afeea8e6a3f41a98
John Mamish <john.mamish@gmail.com>
2020-08-24 01:00:48 -0500

    wip

--- milliLearn/hm01b0/hm01b0_top.v
+++ milliLearn/hm01b0/hm01b0_top.v
@@ -1,5 +1,155 @@
 `timescale 1ns/100ps
 
+/**
+ * What sort of frame synchronization information do I want to come out of the ingester? I suppose
+ * that I could just pass vsync through.
+ *
+ * This assumes that the clock runs at a considerably higher rate than pixclk... at least 6x pixclk.
+ */
+module ingester(input                    clock,
+                input                    reset,
+
+                input [7:0]              hm01b0_pixdata,
+                input                    hm01b0_pixclk,
+                input                    hm01b0_hsync,
+                input                    hm01b0_vsync,
+
+                output reg               vsync,
+                output reg               data_valid,
+                output reg [7:0]         data);
+    parameter image_width = 320;
+    parameter image_height = 240;
+    parameter x_start_padding = 2;
+    parameter x_end_padding = 2;
+    parameter y_start_padding = 2;
+    parameter y_end_padding = 2;
+
+    reg hm01b0_pixclk_prev [0:1];
+    reg hm01b0_hsync_prev [0:1];
+    reg hm01b0_vsync_prev [0:1];
+    reg [7:0] hm01b0_pixdata_prev [0:1];
+
+    //
+    always @(posedge clock) begin
+        if (!reset) begin
+            hm01b0_pixclk_prev[0] <= hm01b0_pixclk;
+            hm01b0_pixclk_prev[1] <= hm01b0_pixclk[0];
+            hm01b0_pixdata_prev[0] <= hm01b0_pixdata;
+            hm01b0_pixdata_prev[1] <= hm01b0_pixdata_prev[0];
+            hm01b0_hsync_prev[0] <= hm01b0_hsync;
+            hm01b0_hsync_prev[1] <= hm01b0_hsync_prev[0];
+            hm01b0_vsync_prev[0] <= hm01b0_vsync;
+            hm01b0_vsync_prev[1] <= hm01b0_vsync_prev[0];
+        end else begin
+            hm01b0_pixclk_prev[0] <= 'h0; hm01b0_pixclk_prev[1] <= 'h0;
+            hm01b0_pixdata_prev[0] <= 'h0; hm01b0_pixdata_prev[1] <= 'h0;
+            hm01b0_hsync_prev[0] <= 'h0; hm01b0_hsync_prev[1] <= 'h0;
+            hm01b0_vsync_prev[0] <= 'h0; hm01b0_vsync_prev[1] <= 'h0;
+        end
+    end
+
+    reg pixclk_posedge;
+    always @* begin
+        pixclk_posedge = !hm01b0_pixclk_prev[0] && hm01b0_pixclk;
+    end
+
+    reg [($clog2(image_width) - 1):0] x_idx;
+    reg [($clog2(image_height) - 1):0] y_idx;
+    always @(posedge clock) begin
+        if (!vsync) begin
+            x_idx <= 'h0;
+            y_idx <= 'h0;
+            data_valid <= 1'b0;
+            data <= 8'hxx;
+        end else if (pixclk_posedge) begin
+            if (x_idx == (image_width + x_start_padding + x_end_padding - 1)) begin
+                x_idx <= 'h0;
+                if (y_idx == (image_height + y_start_padding + y_end_padding - 1)) begin
+                    y_idx <= 'h0;
+                end else begin
+                    y_idx <= y_idx + 'h1;
+                end
+            end else begin
+              x_idx <= x_idx + 'h1;
+            end
+
+            if ((x_idx >= x_start_padding) && (x_idx < (image_width + x_start_padding)) &&
+                (y_idx >= y_start_padding) && (y_idx < (image_height + y_start_padding))) begin
+                data_valid <= 1'b1;
+                data <= hm01b0_pixdata_prev[1];
+            end else begin
+                data_valid <= 1'b0;
+                data <= 8'hxx;
+            end
+        end else begin
+            x_idx <= x_idx;
+            y_idx <= y_idx;
+            data_valid <= 1'b0;
+            data <= 8'hxx;
+        end
+    end
+endmodule
+
+
+/**
+ * Plug an image into the front end, and it will echo the image, but with a sequence added at the
+ * end of each frame, for an end-of-frame alignment marker
+ */
+module frame_end_stuffer(input                      clock,
+                         input                      reset,
+
+                         input                      data_in_valid,
+                         input                      vsync,
+                         input [7:0]                data_in,
+
+                         output reg                 data_out_valid,
+                         output reg [7:0]           data_out);
+    localparam delimiter_length = 32;
+    localparam [(delimiter_length - 1):0] delimiter = 32'hf00f_ba11;
+    localparam delimiter_index_maxvalue = (delimiter_length / 8) - 1;
+
+    reg vsync [0:1];
+
+    always @* begin
+        vsync[0] = vsync;
+    end
+
+    localparam STATE_IMAGE = 1'b0;
+    localparam STATE_DELIMITER = 1'b1;
+    reg state;
+    reg [($clog2(delimiter_index_maxvalue) - 1):0] delimiter_index;
+    always @(posedge clock) begin
+        vsync[1] <= vsync[0];
+
+        case (state)
+            STATE_IMAGE: begin
+                data_out_valid <= data_in_valid;
+                data_out <= data_in;
+                if (!vsync[0] && vsync[1]) begin
+                    state <= STATE_DELIMITER;
+                end else begin
+                    state <= state;
+                end
+                delimiter_index <= 'h0;
+            end
+
+            STATE_DELIMITER: begin
+                data_out_valid <= 1'b1;
+                data_out <= delimiter[(delimiter_index * 8) +: 8];
+
+                if (delimiter_index == delimiter_index_maxvalue) begin
+                    delimiter_index <= 'h0;
+                    state <= STATE_IMAGE;
+                end else begin
+                    delimiter_index <= delimiter_index + 'h1;
+                    state <= state;
+                end
+            end
+        endcase
+    end
+endmodule
+
+
 module hm01b0_top(input                  osc_12m,
 
                   // hm01b0 connections
@@ -22,6 +172,8 @@ module hm01b0_top(input                  osc_12m,
                   output [5:0] gpio,
 
                   output gpio43_hiz);
+    ///////////////////////////////////////////////
+    // Debug GPIOs
     assign config_ps = 1'b1;
 
     assign hm01b0_trig_debug_led = uart_tx_config_copi;
@@ -32,10 +184,12 @@ module hm01b0_top(input                  osc_12m,
     assign gpio[4] = hm01b0_vsync;
     assign gpio[5] = uart_tx_config_copi;*/
 
-    assign gpio[4:0] = hm01b0_pixdata[7:3];
+    assign gpio[2:0] = {hm01b0_vsync, hm01b0_hsync, hm01b0_pixclk};
+
+    /*assign gpio[4:0] = hm01b0_pixdata[7:3];
     assign gpio[5]   = hm01b0_pixclk;
     assign gpio[6]   = hm01b0_vsync;
-    assign gpio[7]   = hm01b0_hsync;
+    assign gpio[7]   = hm01b0_hsync;*/
 
     assign gpio43_hiz = (hm01b0_vsync) ? (1'bz) : (1'b0);
 
@@ -144,7 +298,7 @@ module hm01b0_top(input                  osc_12m,
     end
 
     wire clk_0_230400;
-    divide_by_n #(.N(52)) div_0_230400(.clk(osc_12m), .reset(1'b0), .out(clk_0_230400));
+    divide_by_n #(.N(6)) div_0_230400(.clk(osc_12m), .reset(1'b0), .out(clk_0_230400));
 
     reg uart_strobe;
     reg [7:0] uart_data;
