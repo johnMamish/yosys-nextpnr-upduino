453dac1c37fb15a3c66547d7e41c85f62eae95ae
John Mamish <john.mamish@gmail.com>
2020-08-30 01:50:39 -0500

    move i2c initializer to submodule

--- milliLearn/hm01b0/hm01b0_top.v
+++ milliLearn/hm01b0/hm01b0_top.v
@@ -171,8 +171,75 @@ module frame_end_stuffer(input                      clock,
     end
 endmodule
 
+module i2c_initializer(input                  clock,
+                       input                  reset,
+                       inout                  hm01b0_sda,
+                       inout                  hm01b0_scl);
+    wire sda_in, sda_o, scl_in, scl_o;
+    assign hm01b0_sda = sda_o ? 1'bz : 1'b0;
+    assign sda_in = hm01b0_sda;
+    assign hm01b0_scl = scl_o ? 1'bz : 1'b0;
+    assign scl_in = hm01b0_scl;
 
+    wire [6:0] i2c_init_cmd_address;
+    wire i2c_init_cmd_start, i2c_init_cmd_read, i2c_init_cmd_write, i2c_init_cmd_write_multiple;
+    wire i2c_init_cmd_stop, i2c_init_cmd_valid, i2c_init_cmd_ready;
+    wire [7:0] i2c_init_data_controller_to_peripheral;
+    wire i2c_init_data_controller_to_peripheral_valid, i2c_init_data_controller_to_peripheral_ready;
+    wire i2c_init_data_controller_to_peripheral_last;
+    wire i2c_init_busy, i2c_init_start;
+    i2c_init i2c_init(.clk(clock),
+                      .rst(reset),
+                      .cmd_address(i2c_init_cmd_address),
+                      .cmd_start(i2c_init_cmd_start),
+                      .cmd_read(i2c_init_cmd_read),
+                      .cmd_write(i2c_init_cmd_write),
+                      .cmd_write_multiple(i2c_init_cmd_write_multiple),
+                      .cmd_stop(i2c_init_cmd_stop),
+                      .cmd_valid(i2c_init_cmd_valid),
+                      .cmd_ready(i2c_init_cmd_ready),
 
+                      .data_out(i2c_init_data_controller_to_peripheral),
+                      .data_out_valid(i2c_init_data_controller_to_peripheral_valid),
+                      .data_out_ready(i2c_init_data_controller_to_peripheral_ready),
+                      .data_out_last(i2c_init_data_controller_to_peripheral_last),
+
+                      .busy(i2c_init_busy),
+                      .start(i2c_init_start));
+
+    pulse_one i2c_init_start_pulse(.clock(clock), .reset(reset), .pulse(i2c_init_start));
+    defparam i2c_init_start_pulse.pulse_delay = 50000;
+    defparam i2c_init_start_pulse.pulse_width = 4;
+
+    i2c_master i2c_master(.clk(clock),
+                          .rst(reset),
+                          .cmd_address(i2c_init_cmd_address),
+                          .cmd_start(i2c_init_cmd_start),
+                          .cmd_read(i2c_init_cmd_read),
+                          .cmd_write(i2c_init_cmd_write),
+                          .cmd_write_multiple(i2c_init_cmd_write_multiple),
+                          .cmd_stop(i2c_init_cmd_stop),
+                          .cmd_valid(i2c_init_cmd_valid),
+                          .cmd_ready(i2c_init_cmd_ready),
+
+                          .data_in(i2c_init_data_controller_to_peripheral),
+                          .data_in_valid(i2c_init_data_controller_to_peripheral_valid),
+                          .data_in_ready(i2c_init_data_controller_to_peripheral_ready),
+                          .data_in_last(i2c_init_data_controller_to_peripheral_last),
+
+                          .data_out(),
+                          .data_out_valid(),
+                          .data_out_ready(1'bz),
+                          .data_out_last(),
+
+                          .scl_i(scl_in), .scl_o(scl_o), .scl_t(),
+                          .sda_i(sda_in), .sda_o(sda_o), .sda_t(),
+
+                          // unused
+                          .busy(), .bus_control(), .bus_active(), .missed_ack(),
+
+                          .prescale(16'd16), .stop_on_idle(1'b1));
+endmodule
 
 module hm01b0_top(input                  osc_12m,
 
@@ -224,70 +291,7 @@ module hm01b0_top(input                  osc_12m,
 
     ////////////////////////////////////////////////
     // i2c
-    wire sda_in, sda_o, scl_in, scl_o;
-    assign hm01b0_sda = sda_o ? 1'bz : 1'b0;
-    assign sda_in = hm01b0_sda;
-    assign hm01b0_scl = scl_o ? 1'bz : 1'b0;
-    assign scl_in = hm01b0_scl;
-
-    wire [6:0] i2c_init_cmd_address;
-    wire i2c_init_cmd_start, i2c_init_cmd_read, i2c_init_cmd_write, i2c_init_cmd_write_multiple;
-    wire i2c_init_cmd_stop, i2c_init_cmd_valid, i2c_init_cmd_ready;
-    wire [7:0] i2c_init_data_controller_to_peripheral;
-    wire i2c_init_data_controller_to_peripheral_valid, i2c_init_data_controller_to_peripheral_ready;
-    wire i2c_init_data_controller_to_peripheral_last;
-    wire i2c_init_busy, i2c_init_start;
-    i2c_init i2c_init(.clk(osc_12m),
-                      .rst(reset),
-                      .cmd_address(i2c_init_cmd_address),
-                      .cmd_start(i2c_init_cmd_start),
-                      .cmd_read(i2c_init_cmd_read),
-                      .cmd_write(i2c_init_cmd_write),
-                      .cmd_write_multiple(i2c_init_cmd_write_multiple),
-                      .cmd_stop(i2c_init_cmd_stop),
-                      .cmd_valid(i2c_init_cmd_valid),
-                      .cmd_ready(i2c_init_cmd_ready),
-
-                      .data_out(i2c_init_data_controller_to_peripheral),
-                      .data_out_valid(i2c_init_data_controller_to_peripheral_valid),
-                      .data_out_ready(i2c_init_data_controller_to_peripheral_ready),
-                      .data_out_last(i2c_init_data_controller_to_peripheral_last),
-
-                      .busy(i2c_init_busy),
-                      .start(i2c_init_start));
-
-    pulse_one i2c_init_start_pulse(.clock(osc_12m), .reset(reset), .pulse(i2c_init_start));
-    defparam i2c_init_start_pulse.pulse_delay = 50000;
-    defparam i2c_init_start_pulse.pulse_width = 4;
-
-    i2c_master i2c_master(.clk(osc_12m),
-                          .rst(reset),
-                          .cmd_address(i2c_init_cmd_address),
-                          .cmd_start(i2c_init_cmd_start),
-                          .cmd_read(i2c_init_cmd_read),
-                          .cmd_write(i2c_init_cmd_write),
-                          .cmd_write_multiple(i2c_init_cmd_write_multiple),
-                          .cmd_stop(i2c_init_cmd_stop),
-                          .cmd_valid(i2c_init_cmd_valid),
-                          .cmd_ready(i2c_init_cmd_ready),
-
-                          .data_in(i2c_init_data_controller_to_peripheral),
-                          .data_in_valid(i2c_init_data_controller_to_peripheral_valid),
-                          .data_in_ready(i2c_init_data_controller_to_peripheral_ready),
-                          .data_in_last(i2c_init_data_controller_to_peripheral_last),
-
-                          .data_out(),
-                          .data_out_valid(),
-                          .data_out_ready(1'bz),
-                          .data_out_last(),
-
-                          .scl_i(scl_in), .scl_o(scl_o), .scl_t(),
-                          .sda_i(sda_in), .sda_o(sda_o), .sda_t(),
-
-                          // unused
-                          .busy(), .bus_control(), .bus_active(), .missed_ack(),
-
-                          .prescale(16'd16), .stop_on_idle(1'b1));
+    i2c_initializer initializer(.clock(osc_12m), .reset(reset), .hm01b0_sda(hm01b0_sda), .hm01b0_scl(hm01b0_scl));
 
     ////////////////////////////////////////////////
     // hm01b0 mck
